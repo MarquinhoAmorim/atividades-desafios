5ac241f4997202e1b4b2e019b13a292eb32d7e73
MarquinhoAmorim <marcoamorim877@gmail.com>
2024-05-11 18:30:14 -0300

    Lista de Tarefas | Atualizada

--- C++/lista.cpp
+++ C++/lista.cpp
@@ -117,37 +117,35 @@ void inserir_dados(Lista *lista) {
     }
 }
 
-TAREFA retirar_tarefa_por_posicao(Lista *lista, int posicao) { 
-    TAREFA retorno;
-    cout << "Tamanho: " << lista->tamanho <<endl;
-    cout << "Posição: " << posicao <<endl;
-    if (lista->tamanho <= posicao){
-        cout << endl << "Tamanho da lista menor que a posição informada" << endl;
-    } else {
-        Node* aux = lista->inicio;
-        while (posicao > 0 && aux != NULL) {
-            posicao--;
-            if (posicao == 0) {
-                cout <<  endl << "Remover: " << aux->tarefa.tarefa << endl;
-                if (aux->anterior != NULL)
-                    aux->anterior->proximo = aux->proximo;
-                if (aux->proximo != NULL)
-                    aux->proximo->anterior = aux->anterior;
-                if (aux == lista->inicio)
-                    lista->inicio = aux->proximo;
-                if (aux == lista->final)
-                    lista->final = aux->anterior;
-                aux->proximo = NULL;
-                aux->anterior = NULL;
-                retorno = aux->tarefa;
-                lista->tamanho = lista->tamanho - 1;
-                delete(aux);
-                return retorno;
-            }
-            aux = aux->proximo;
-        }
-    }
-    return retorno;
+TAREFA retirar_tarefa_por_posicao(Lista *lista, int posicao) {
+	TAREFA retorno;
+//		cout << "Tamanho: " << lista->tamanho <<endl;
+//  	cout << "Posi��o: " << posicao <<endl;
+	if ( lista->tamanho < posicao){
+	    cout << endl << "Tamanho da lista menor que a posição informada" << endl;
+	}
+	if(lista->tamanho != 0){
+		Node* aux = lista->inicio;
+		while (posicao >= 0) {
+		    posicao--;
+		    if (posicao == 0) {
+		        aux->proximo->anterior = aux->anterior;
+		        if(aux->anterior != NULL){
+		            aux->anterior->proximo = aux->proximo;
+		        }else{
+		            lista->inicio = aux->proximo;
+				}
+		        aux->proximo = NULL;
+		        aux->anterior = NULL;
+		        retorno = aux->tarefa;
+		        lista->tamanho = lista->tamanho - 1;
+		        delete(aux);
+		        return retorno;
+		    }
+		    aux = aux->proximo;
+		}
+	}
+	return retorno;
 }
 
 void adicionar_tarefa(Lista *lista, TAREFA tarefa){
@@ -160,14 +158,23 @@ void adicionar_tarefa(Lista *lista, TAREFA tarefa){
     new_node->proximo = NULL;
     new_node->anterior = NULL;
 
-     if (lista->inicio == NULL) {
+   if (lista->inicio == NULL) {
         lista->inicio = new_node;
         lista->final = new_node;
         lista->tamanho = lista->tamanho + 1;
         return;
-    } else {
+    } 
+    if (lista->tamanho == 1) {
+        Node* aux = lista->final;
         lista->final->proximo = new_node;
         new_node->anterior = lista->final;
+        lista->final = new_node; 
+        lista->tamanho = lista->tamanho + 1;
+        return;
+    } else {
+        Node* aux = lista->final;
+        aux->proximo = new_node;
+        new_node->anterior = aux;
         lista->final = new_node; 
         lista->tamanho = lista->tamanho + 1;
         return;
@@ -201,119 +208,96 @@ void mostrar_dados_tarefa(TAREFA tarefa){
 
 /* Funções devem ser desenvolvidas a partir daqui */
 
-void retirar_tarefa_por_posicao2(Lista *lista, int categoria)
+void retirar_tarefa_por_posicao2(Lista *lista, int posicao)
 {
-    Node* aux = lista->inicio;
-    Node* temp;
-
-    while (aux != NULL) {
-        if (categoria == 1 && aux->tarefa.categoria <= 15) {
-            temp = aux;
-            aux = aux->proximo;
-
-            if (temp->anterior != NULL)
-                temp->anterior->proximo = temp->proximo;
-            if (temp->proximo != NULL)
-                temp->proximo->anterior = temp->anterior;
-            if (temp == lista->inicio)
-                lista->inicio = temp->proximo;
-            if (temp == lista->final)
-                lista->final = temp->anterior;
-
-            temp->anterior = NULL;
-            temp->proximo = NULL;
-            delete(temp);
-            lista->tamanho--;
-        } else if (categoria == 2 && aux->tarefa.categoria > 15) {
-            temp = aux;
-            aux = aux->proximo;
-
-            if (temp->anterior != NULL)
-                temp->anterior->proximo = temp->proximo;
-            if (temp->proximo != NULL)
-                temp->proximo->anterior = temp->anterior;
-            if (temp == lista->inicio)
-                lista->inicio = temp->proximo;
-            if (temp == lista->final)
-                lista->final = temp->anterior;
-
-            temp->anterior = NULL;
-            temp->proximo = NULL;
-            delete(temp);
-            lista->tamanho--;
-        } else {
-            aux = aux->proximo;
-        }
-    }
+    if(posicao > lista->tamanho){
+		cerr << "Tamanho da lista menor que a posição informada"<<endl;
+		return;
+	}
+	if(lista->tamanho != 0){
+		Node *aux = lista->inicio;
+		for(int x = 0; x < posicao - 1; x++){
+			aux = aux->proximo;
+		}
+		if(aux->anterior != NULL){
+			aux->anterior->proximo = aux->proximo;
+		}else{
+			lista->inicio = aux->proximo;
+		}
+		aux->proximo->anterior = aux->anterior;
+		aux->proximo = NULL;
+		aux->anterior = NULL;
+		cout << "Removido:    "<<aux->tarefa.tarefa;
+        cout << endl;
+		delete aux;
+	}
 }
 
 void buscar_tarefas_por_categoria(Lista *lista, int categoria)
 {
+    if(lista->tamanho!=0)
+    {
+		Node *aux = lista->inicio;
+		if(categoria == 1) {
+			cout << "TAREFAS NA CATEGORIA 1" << endl;
+			while(aux != NULL){
+				if(aux->tarefa.categoria == 1){
+					cout << "[" << aux->tarefa.tarefa << "]";
+				}
+				aux = aux->proximo;
+			}
 
-    Node* aux = lista->inicio;
+		} else {
+			cout << "TAREFAS NA CATEGORIA 2" << endl;
+			while(aux != NULL){
+				if(aux->tarefa.categoria == 2){
+					cout << "[" <<aux->tarefa.tarefa << "]";
+				}
+				aux = aux->proximo;
+			}
 
-    if (categoria == 1) {
-        cout << "TAREFAS NA CATEGORIA 1" << endl;
-        while (aux != NULL) {
-            if (aux->tarefa.categoria <= 15) {
-                cout << aux->tarefa.tarefa << endl;
-            }
-            aux = aux->proximo;
-        }
-    } else if (categoria == 2) {
-        cout << "TAREFAS NA CATEGORIA 2" << endl;
-        while (aux != NULL) {
-            if (aux->tarefa.categoria > 15) {
-                cout << aux->tarefa.tarefa << endl;
-            }
-            aux = aux->proximo;
-        }
-    } else {
-        cout << "Categoria inválida." << endl;
-    }
+			cout << endl;
+		}
+	}
     
 }
 
 
 void buscar_tarefas_por_prioridades(Lista *lista, int prioridade)
 {
-    Node* aux = lista->inicio;
-
-    if (prioridade == 1) {
-        cout << "TAREFAS COM PRIORIDADE 1" << endl;
-        while (aux != NULL) {
-            if (aux->tarefa.prioridade <= 10) {
-                cout << aux->tarefa.tarefa << endl;
-            }
-            aux = aux->proximo;
-        }
-    } else if (prioridade == 2) {
-        cout << "TAREFAS COM PRIORIDADE 2" << endl;
-        while (aux != NULL) {
-            if (aux->tarefa.prioridade > 10) {
-                cout << aux->tarefa.tarefa << endl;
-            }
-            aux = aux->proximo;
-        }
-    } else {
-        cout << "Prioridade inválida." << endl;
-    }
+    if(lista->tamanho != 0) 
+    {
+		Node *aux = lista->inicio;
+		if(prioridade == 1){
+			cout << "TAREFAS COM PRIORIDADE 1" << endl;
+			while(aux != NULL){
+				if(aux->tarefa.prioridade == 1){
+					cout << "[" << aux->tarefa.tarefa << "]";
+				}
+				aux = aux->proximo;
+			}
+			cout << endl;
+		}else{
+            cout << endl;
+			cout << "TAREFAS COM PRIORIDADE 2" << endl;
+			while(aux != NULL){
+				if(aux->tarefa.prioridade == 2){
+					cout << "["<<aux->tarefa.tarefa << "]";
+				}
+				aux = aux->proximo;
+			}
+			cout << endl;
+		}
+	}
 }
 
 void encerrar(Lista *lista) 
 {   
-    Node* aux = lista->inicio;
-    Node* temp;
-
-    while (aux != NULL) {
-        temp = aux;
-        aux = aux->proximo;
-        delete temp;
-    }
-
-    lista->inicio = NULL;
-    lista->final = NULL;
-    lista->tamanho = 0;
-
-    cout << "Memória desalocada." << endl;
+   Node *aux = lista->inicio;
+	while(lista->tamanho != 0){
+		Node *deleteAux = aux;
+		aux = aux->proximo;
+		delete deleteAux;
+		lista->tamanho--;
+	}
 }
